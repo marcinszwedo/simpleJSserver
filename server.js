06073fe728c1a0b8903f0ad48e34fe2cbfe0bfc1
Jureks <uzytkownik@kursuSDA.pl>
2023-10-25 20:37:33 +0200

    Zmiana w pliku server na mega

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
     res.writeHead(200);
-    res.end('Grupa ZDTESTpol137 jest super SUPER!');
+    res.end('Grupa ZDTESTpol137 jest mega SUPER!');
 }
 
 const server = http.createServer(requestListener);
